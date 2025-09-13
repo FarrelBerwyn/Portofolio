a46f5f15fcefdf2c28bd413a0a749181e50272e4
Farrel Berwyn <farrel.berwyn18@gmail.com>
2025-09-14 06:40:39 +0700

    Simplify app to show only Main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import React, {useState, useEffect} from "react";
-import {
-  Main,
-  Timeline,
-  Expertise,
-  Project,
-  Contact,
-  Navigation,
-  Footer,
-} from "./components";
-import FadeIn from './components/FadeIn';
+import React, {useState} from "react";
+import { Main } from "./components";
 import './index.scss';
 
 function App() {
@@ -22,21 +13,9 @@ function App() {
         }
     }
 
-    useEffect(() => {
-        window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-      }, []);
-
     return (
     <div className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}>
-        <Navigation parentToChild={{mode}} modeChange={handleModeChange}/>
-        <FadeIn transitionDuration={700}>
-            <Main/>
-            <Expertise/>
-            <Timeline/>
-            <Project/>
-            <Contact/>
-        </FadeIn>
-        <Footer />
+        <Main />
     </div>
     );
 }
