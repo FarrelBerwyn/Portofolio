17a978cbf199cf551d05defbfda9b3891037ee22
Farrel Berwyn <farrel.berwyn18@gmail.com>
2025-09-14 06:25:53 +0700

    Update profile information and social links

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,19 +9,19 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src="https://media.licdn.com/dms/image/v2/D5603AQFHC7oJ2O0IWQ/profile-displayphoto-shrink_800_800/B56ZbDaobgGkAc-/0/1747035262461?e=1760572800&v=beta&t=9SXTIGdrNwPijLz2xmHIt8Gb08sy7Vh_szcrH6eR4Bw" alt="Avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/FarrelBerwyn" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/farrel-berwyn" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Farrel Chandra Berwyn</h1>
           <p>Startup Founder & Front End Engineer</p>
 
           <div className="mobile_social_icons">
-            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="https://github.com/FarrelBerwyn" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/farrel-berwyn" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
         </div>
       </div>
